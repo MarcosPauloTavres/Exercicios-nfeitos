8148814ab24fdb69507426359a4d7d6bd064ead1
MarcosPauloTl <102967361+MarcosPauloTavres@users.noreply.github.com>
2025-11-12 18:35:57 -0300

    Add files via upload

---
+++ logica-js-projeto_inicial (1)/logica-js-projeto_inicial/app.js
@@ -0,0 +1,11 @@
+alert ('Seja muito bem vindo');
+
+let saldoDisponivel = 1000;
+alert ('Erro! preencha todos os campos');
+let mensagemDeErro = ('Erro prencha todos os campos');
+alert (mensagemDeErro);
+let nome = prompt('Qual é sua nome?'); 
+let idade = prompt('Qual é sua idade')
+if (idade > 18){
+    alert('pode tirar abilitaçao')
+}
