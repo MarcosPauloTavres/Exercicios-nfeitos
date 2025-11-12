c4f190768d82db5c6c0deb4a9e7a4d16fa626cb8
MarcosPauloTl <102967361+MarcosPauloTavres@users.noreply.github.com>
2025-11-12 19:39:35 -0300

    Refactor app.js for user prompts and alerts

--- logica-js-projeto_inicial (1)/logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial (1)/logica-js-projeto_inicial/app.js
@@ -1,4 +1,32 @@
-alert ('Seja muito bem vindo');
+Perguntas
+
+1 Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
+
+2 Declare uma variável chamada nome e atribua a ela o valor "Lua".
+
+3 Crie uma variável chamada idade e atribua a ela o valor 25.
+
+4 Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+
+5 Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
+
+6 Exiba um alerta com o texto "Erro! Preencha todos os campos"
+
+7 Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
+
+8 Para o próximo código, use um novo prompt para perguntar o nome do usuário e armazená-lo em uma variável, pode chamá-la de nome ou adicionar o que desejar .
+
+9 Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
+
+10 Agora, para validar a idade que capturamos no desafio 09, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
+
+resposta
+
+1 alert ('Seja muito bem vindo ao nosso site ');
+2 let nome = 'lua';
+3 let idade = 25;
+4
+    
 
 let saldoDisponivel = 1000;
 alert ('Erro! preencha todos os campos');
@@ -8,4 +36,5 @@ let nome = prompt('Qual é sua nome?');
 let idade = prompt('Qual é sua idade')
 if (idade > 18){
     alert('pode tirar abilitaçao')
+
 }
